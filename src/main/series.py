3aff19f9628ab41bf5d576cdb4dd59d5b8e251ab
Tjorriemorrie <jacoj82@gmail.com>
2015-01-13 17:19:36 +0200

    Series better parsing

--- src/main/series.py
+++ src/main/series.py
@@ -9,21 +9,31 @@ import re
 class Series():
     def extract(self):
         logging.info('series: extraction running...')
-        cutoff = arrow.utcnow().replace(days=-7).datetime
-        torrents = Torrent.query(Torrent.category == 'tv', Torrent.created_at > cutoff, Torrent.series_title == None).fetch(10)
+        torrents = Torrent.query(Torrent.category == 'tv', Torrent.series_title == None).fetch()
         logging.info('{0} torrents fetched'.format(len(torrents)))
-        results = {}
+        results = []
         for torrent in torrents:
-            title_groups = re.match(r'(.*)(s\d{1,2})(e\d{1,2})', torrent.title, re.I)
+            title_groups = re.match(r'(.*)(s\d{1,2})(e\d{1,2}\s)', torrent.title, re.I)
             if title_groups is not None:
                 torrent.series_title = title_groups.group(1).replace('.', ' ').strip()
                 torrent.series_season = int(title_groups.group(2)[1:])
                 torrent.series_episode = int(title_groups.group(3)[1:])
                 torrent.put()
                 # pprint(torrent)
-                results[torrent.title] = 'found as {0} S{1} E{2}'.format(torrent.series_title, torrent.series_season, torrent.series_episode)
+                msg = '[200] {0} S{1} E{2}'.format(torrent.series_title, torrent.series_season, torrent.series_episode)
             else:
-                results[torrent.title] = 'Not found'
+                title_groups = re.match(r'(.*)(e\d{1,3}\s)', torrent.title, re.I)
+                if title_groups is not None:
+                    torrent.series_title = title_groups.group(1).replace('.', ' ').strip()
+                    torrent.series_season = None
+                    torrent.series_episode = int(title_groups.group(2)[1:])
+                    torrent.put()
+                    # pprint(torrent)
+                    msg = '[200] {0} E{1}'.format(torrent.series_title, torrent.series_episode)
+                else:
+                    msg = '[404] {0}'.format(torrent.title)
+            results.append(msg)
+            logging.info(msg)
         self.notify(results)
 
 
@@ -32,5 +42,5 @@ class Series():
             sender='jacoj82@gmail.com',
             to='jacoj82@gmail.com',
             subject="Series extracted",
-            body='\n'.join(['{0} {1}'.format(k, v) for k, v in results.iteritems()]),
+            body='\n'.join(results),
         )
