96cb47c112dfa78b06f204bc5c17ac10ae0a06ff
tjorriemorrie <jacoj82@gmail.com>
2015-11-10 23:15:02 +0200

    Parsed series

--- src/main/series.py
+++ src/main/series.py
@@ -14,64 +14,70 @@ class Series():
         logging.info('{0} torrents fetched'.format(len(torrents)))
         results = []
         for torrent in torrents:
-            logging.info('parsing {0}...'.format(torrent.title.encode('utf-8')))
-
-            # plain and simple e.g. xxx S##E##
-            title_groups = re.match(r'(.*)\s(s\d{1,2})(e\d{1,2})\s', torrent.title.replace('.', ' ').strip(), re.I)
-            if title_groups is not None:
-                logging.info('series and episode found {0}'.format(title_groups.groups()))
-                torrent.series_title = title_groups.group(1)
-                torrent.series_season = int(title_groups.group(2)[1:])
-                torrent.series_episode = int(title_groups.group(3)[1:])
-                torrent.put()
-                # pprint(torrent)
-                msg = '[200] {0} S{1} E{2}'.format(torrent.series_title, torrent.series_season, torrent.series_episode)
-            else:
-                logging.info('series and episode not found')
-
-                # only episode given e.g. xxx E###
-                title_groups = re.match(r'(.*)\s(e\d{1,3})\s', torrent.title.replace('.', ' ').strip(), re.I)
-                if title_groups is not None:
-                    logging.info('only episode found')
-                    torrent.series_title = title_groups.group(1).replace('.', ' ').strip()
-                    torrent.series_season = None
-                    torrent.series_episode = int(title_groups.group(2)[1:])
-                    torrent.put()
-                    # pprint(torrent)
-                    msg = '[200] {0} E{1}'.format(torrent.series_title, torrent.series_episode)
-                else:
-                    logging.info('only episode not found')
-
-                    # simple version e.g. ##x##_
-                    title_groups = re.match(r'(.*)(\d{1,2})x(\d{1,2})\s', torrent.title.replace('.', ' ').strip(), re.I)
-                    if title_groups is not None:
-                        logging.info('series x episode found')
-                        torrent.series_title = title_groups.group(1).replace('.', ' ').strip()
-                        torrent.series_season = int(title_groups.group(2))
-                        torrent.series_episode = int(title_groups.group(3))
-                        torrent.put()
-                        # pprint(torrent)
-                        msg = '[200] <= {0} S{1} E{2}'.format(torrent.series_title, torrent.series_season, torrent.series_episode)
-                    else:
-                        logging.info('simple version not found')
-
-                        # pilot?
-                        title_groups = re.match(r'(.*)(-pilot)', torrent.title.replace('.', ' ').strip(), re.I)
-                        if title_groups is not None:
-                            logging.info('pilot episode found')
-                            torrent.series_title = title_groups.group(1).replace('.', ' ').strip()
-                            torrent.series_season = 1
-                            torrent.series_episode = 1
-                            torrent.put()
-                            # pprint(torrent)
-                            msg = '[200] <= {0} S{1} E{2}'.format(torrent.series_title, torrent.series_season, torrent.series_episode)
-                        else:
-                            logging.info('absolutely not found')
-                            msg = '[404] <= {0}'.format(torrent.title.encode('utf-8'))
+            try:
+                msg = self.parseSeries(torrent)
+            except Exception as e:
+                msg = e.message
             results.append(msg)
             logging.info(msg)
         self.notify(results)
 
+    def parseSeries(self, torrent):
+        logging.info('parsing {0}...'.format(torrent.title.encode('utf-8')))
+
+        # plain and simple e.g. xxx S##E##
+        title_groups = re.match(r'(.*)\s(s\d{1,2})(e\d{1,2})\s', torrent.title.replace('.', ' ').strip(), re.I)
+        if title_groups is not None:
+            logging.info('series and episode found {0}'.format(title_groups.groups()))
+            torrent.series_title = title_groups.group(1)
+            torrent.series_season = int(title_groups.group(2)[1:])
+            torrent.series_episode = int(title_groups.group(3)[1:])
+            torrent.put()
+            # pprint(torrent)
+            msg = '[200] {0} S{1} E{2}'.format(torrent.series_title.encode('utf-8'), torrent.series_season, torrent.series_episode)
+        else:
+            logging.info('series and episode not found')
+
+            # only episode given e.g. xxx E###
+            title_groups = re.match(r'(.*)\s(e\d{1,3})\s', torrent.title.replace('.', ' ').strip(), re.I)
+            if title_groups is not None:
+                logging.info('only episode found')
+                torrent.series_title = title_groups.group(1).replace('.', ' ').strip()
+                torrent.series_season = None
+                torrent.series_episode = int(title_groups.group(2)[1:])
+                torrent.put()
+                # pprint(torrent)
+                msg = '[200] {0} E{1}'.format(torrent.series_title.encode('utf-8'), torrent.series_episode)
+            else:
+                logging.info('only episode not found')
+
+                # simple version e.g. ##x##_
+                title_groups = re.match(r'(.*)(\d{1,2})x(\d{1,2})\s', torrent.title.replace('.', ' ').strip(), re.I)
+                if title_groups is not None:
+                    logging.info('series x episode found')
+                    torrent.series_title = title_groups.group(1).replace('.', ' ').strip()
+                    torrent.series_season = int(title_groups.group(2))
+                    torrent.series_episode = int(title_groups.group(3))
+                    torrent.put()
+                    # pprint(torrent)
+                    msg = '[200] <= {0} S{1} E{2}'.format(torrent.series_title.encode('utf-8'), torrent.series_season, torrent.series_episode)
+                else:
+                    logging.info('simple version not found')
+
+                    # pilot?
+                    title_groups = re.match(r'(.*)(-pilot)', torrent.title.replace('.', ' ').strip(), re.I)
+                    if title_groups is not None:
+                        logging.info('pilot episode found')
+                        torrent.series_title = title_groups.group(1).replace('.', ' ').strip()
+                        torrent.series_season = 1
+                        torrent.series_episode = 1
+                        torrent.put()
+                        # pprint(torrent)
+                        msg = '[200] <= {0} S{1} E{2}'.format(torrent.series_title.encode('utf-8'), torrent.series_season, torrent.series_episode)
+                    else:
+                        logging.info('absolutely not found')
+                        msg = '[404] <= {0}'.format(torrent.title.encode('utf-8'))
+        return msg
 
     def notify(self, results):
         mail.send_mail(
